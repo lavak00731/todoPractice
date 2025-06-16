d20e4772ccf2ba4897f54e7596647a71adab4b62
Ezequiel <eolavaca@gmail.com>
2025-06-16 09:23:11 -0300

    getting only one task

--- index.js
+++ index.js
@@ -4,8 +4,20 @@ const fs = require('fs');
 const server = express();
 server.use(express.json());
 
+//get all tasks
 server.get('/api/tasks', (req, res)=>{
     res.status(200).json(db)
-})
+});
+
+//get specific task
+server.get('/api/tasks/:id', (req, res)=>{
+    const param  = req.params;
+    const user = db.tasks.find(task => task.id === parseInt(param.id))
+    if(!user) {
+        res.sendStatus(404);
+    }
+    res.status(200).json(user);
+});
+
 
 server.listen('8080', console.log('server listening on 8080'))
